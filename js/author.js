dd535376027b9ebf66d51652e957ddad0524b866
afterglow2025 <afterglow2025@gmail.com>
2025-05-17 18:24:54 +0900

    전체 파일 및 신규 파일 강제 덮어쓰기 반영

--- js/author.js
+++ js/author.js
@@ -1,1 +1,16 @@
-// �۰� ������ ��ũ��Ʈ 
+fetch('authors.json')
+  .then(res => res.json())
+  .then(authors => {
+    const grid = document.getElementById('authorGrid');
+    grid.innerHTML = authors.map(author => `
+      <div class="author-card" data-aos="zoom-in">
+        <img src="${author.image}" alt="${author.name}">
+        <div class="overlay">
+          <h3>${author.name}</h3>
+          <p>${author.position}</p>
+          ${author.email ? `<p>${author.email}</p>` : ''}
+        </div>
+      </div>
+    `).join('');
+  });
+
