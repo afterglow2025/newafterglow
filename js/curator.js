7c27718b006c62eb5939bf28418e4228a5f8d23b
afterglow2025 <afterglow2025@gmail.com>
2025-05-17 20:40:54 +0900

    Add files via upload

--- js/curator.js
+++ js/curator.js
@@ -1,1 +1,22 @@
-// ť������ ������ ��ũ��Ʈ 
+let allAuthors = [];
+
+fetch('curator.json')
+  .then(res => res.json())
+  .then(authors => {
+    allAuthors = authors;
+    renderAuthors(authors);
+  });
+
+function renderAuthors(authors) {
+  const grid = document.getElementById('authorGrid');
+  grid.innerHTML = authors.map(author => `
+    <div class="author-card" data-aos="zoom-in">
+      <img src="${author.image}" alt="${author.name}">
+      <div class="overlay">
+        <h3>${author.name}</h3>
+        <p>${author.position}</p>
+        ${author.email ? `<p>${author.email}</p>` : ''}
+      </div>
+    </div>
+  `).join('');
+}
